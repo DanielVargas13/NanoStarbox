628f8887ba8180df83d16095eabb071bbc77d112
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-04-16 19:30:39 -0500

    https; moved scripting site

--- plugin/RhinoWebDriver/test/box/star/js/RhinoWebDriverTest.java
+++ plugin/RhinoWebDriver/test/box/star/js/RhinoWebDriverTest.java
@@ -3,14 +3,17 @@ package box.star.js;
 import box.star.WebServer;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RhinoWebDriverTest {
     WebServer ws = new WebServer();
     @Test
     void p() throws Exception {
-        ws.setDocumentRoot("../../sample/site");
+        ws.setDocumentRoot("../../sample/site/rhino-web-driver");
         new RhinoWebDriver(ws);
+        ws.makeSecure(new File("../../sample/Starbox.jks"), "Starbox");
         ws.start();
         do {
              Thread.sleep(1000);
