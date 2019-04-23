d941f6a9c1e569150d5f9351dcba862499d77fa3
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-04-23 12:51:51 -0500

    Box Command System: Master System
    Shells, sub-shells, actions, (with background and foreground modes)
    
    inline shell-functions (returning int), java-functions (returning object)
    
    supports shell streams above stdio.
    
    automatic inheritable environment for each Main method.
    
    todo: system runtime exec and pipe, environment access.

---
+++ src/main/test/box/star/system/ShellTest.java
@@ -0,0 +1,68 @@
+package box.star.system;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ShellTest {
+
+    Shell shell = Shell.createShell(new Shell.Main("main") {
+        @Override
+        public int main(String[] parameters) {
+            shell.call("echo", "hello world");
+//            Shell shell2 = shell.createShell(new Shell.Main("test"){
+//                @Override
+//                public int main(String[] parameters) {
+//                    Shell.Action task = shell.createAction("echo");
+//                    return shell.run(task, parameters);
+//                }
+//            });
+//            return shell2.main(parameters);
+            return shell.status;
+        }
+    });
+
+    {
+        shell.traceLifeCycle(true);
+
+
+        shell.registerActionModel(new Shell.Method("echo"){
+            @Override
+            public Object main(Object[] parameters) {
+                String[] stringArray = Arrays.copyOf(parameters, parameters.length, String[].class);
+                Stack<String> print = new Stack<>();
+                print.addAll(Arrays.asList(stringArray));
+                //print.remove(0);
+                boolean lineMode = true;
+                while (print.get(0).startsWith("-")) {
+                    if (print.get(0).equals("-n")) {
+                        lineMode = false;
+                        print.remove(0);
+                        continue;
+                    }
+                    break;
+                }
+                try {
+                    String out = String.join(" ", print) +  "\n";
+                    System.out.write(out.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return 1;
+            }
+        });
+    }
+
+    @Test void getTaskGroup(){
+        assertEquals(null, Shell.ActionGroup.getCurrentGroup());
+    }
+
+    @Test void main() throws InterruptedException {
+        assertEquals(1, shell.main("echo", "hello world"));
+    }
+
+}
