7883eb19f7bff84b5e2028fb1db5c40b1958eea2
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-04-25 12:50:12 -0500

    Box Command System: Functional Shell Model

---
+++ src/main/test/box/star/fn/sh/ShellTest.java
@@ -0,0 +1,30 @@
+package box.star.fn.sh;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class ShellTest {
+
+  Shell shell = new Shell();
+
+  @Test void main() {
+    shell.defineFunction("echo", new Function(){
+      @Override
+      public int main(String[] parameters) {
+        try {
+          stdout.write((parameters[1] + "\n").getBytes());
+        }
+        catch (IOException e) {
+          e.printStackTrace();
+        }
+        return 0;
+      }
+    });
+    Command starbox = shell.build( "java", "-cp", "jar/StarboxShell.jar");
+    Command cat = shell.build("echo", "hello world");
+    //Command cat = starbox.build("box.star.bin.cat", "sample/grep-test.txt");
+    Command grep = starbox.build("box.star.bin.grep", "hello");
+    cat.pipe(grep).run();
+  }
+}
