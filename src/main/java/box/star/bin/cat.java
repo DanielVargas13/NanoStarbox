de50adc4c6b00035f788e641b3d5b6b2e41959fc
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-04-26 17:26:12 -0500

    Code re-format

--- src/main/java/box/star/bin/cat.java
+++ src/main/java/box/star/bin/cat.java
@@ -7,13 +7,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class cat {
-    public static void main(String[] args) {
-        for (String fileName: args){
-            try {
-                System.out.print(Streams.readWholeString(new FileInputStream(new File(fileName))));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+  public static void main(String[] args) {
+    for (String fileName: args){
+      try {
+        System.out.print(Streams.readWholeString(new FileInputStream(new File(fileName))));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+  }
 }
--- src/main/java/box/star/bin/grep.java
+++ src/main/java/box/star/bin/grep.java
@@ -8,49 +8,49 @@ import java.io.*;
 
 public class grep {
 
-    public static void main2(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        try {
-            while ((line = br.readLine()) != null) {
-                for (String expr: args) {
-                    if (line.matches(expr)) {
-                        System.out.println(line);
-                    }
-                }
-            }
-        } catch (Exception e){}
-    }
-
-    public static void main(String[] args) {
-        String line;
-        int lines = 0;
-        int matches = 0;
-
-        Scanner scan = null;
-        scan = new Scanner( System.in );
-
-        // Get a regular expression from the command line
-        Pattern pat = Pattern.compile( args[0] );
-
-        // Match lines against the Regular Expression
-        while ( scan.hasNextLine() )
-        {
-            line = scan.nextLine();
-            lines++;
-
-            // Check if the current line contains the pattern
-            Matcher match = pat.matcher( line );
-            if (match.find())
-            {
-                System.out.println (line);
-                matches++;
-            }
+  public static void main2(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String line;
+    try {
+      while ((line = br.readLine()) != null) {
+        for (String expr: args) {
+          if (line.matches(expr)) {
+            System.out.println(line);
+          }
         }
+      }
+    } catch (Exception e){}
+  }
 
-        System.err.println( "grep-status: " + lines + " lines, " + matches + " matches" );
+  public static void main(String[] args) {
+    String line;
+    int lines = 0;
+    int matches = 0;
 
-        System.out.flush();
+    Scanner scan = null;
+    scan = new Scanner( System.in );
+
+    // Get a regular expression from the command line
+    Pattern pat = Pattern.compile( args[0] );
+
+    // Match lines against the Regular Expression
+    while ( scan.hasNextLine() )
+    {
+      line = scan.nextLine();
+      lines++;
+
+      // Check if the current line contains the pattern
+      Matcher match = pat.matcher( line );
+      if (match.find())
+      {
+        System.out.println (line);
+        matches++;
+      }
     }
+
+    System.err.println( "grep-status: " + lines + " lines, " + matches + " matches" );
+
+    System.out.flush();
+  }
 
 }
