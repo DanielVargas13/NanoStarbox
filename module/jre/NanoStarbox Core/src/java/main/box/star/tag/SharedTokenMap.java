6deb885984cabd80f921e1ae4ec3c9411f384abb
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-01 19:20:24 -0500

    Shared Token Map

---
+++ module/jre/NanoStarbox Core/src/java/main/box/star/tag/SharedTokenMap.java
@@ -0,0 +1,44 @@
+package box.star.tag;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SharedTokenMap<T> {
+
+  protected int[] tokenFormat;
+
+  protected TokenGenerator tokenGenerator = new TokenGenerator();
+  protected ConcurrentHashMap<String, T> map;
+
+  public SharedTokenMap(int... lengths){
+    tokenFormat = lengths;
+    map = new ConcurrentHashMap<>();
+  }
+
+  public SharedTokenMap(int tokenLength){
+    tokenFormat = new int[]{tokenLength};
+    map = new ConcurrentHashMap<>();
+  }
+
+  private String getNextToken(){
+    String token;
+    do { token = tokenGenerator.createNewToken(tokenFormat); }
+    while (map.containsKey(token));
+    return token;
+  }
+
+  synchronized public String put(T value){
+    String token = getNextToken();
+    map.put(token, value);
+    return token;
+  }
+
+  public void set(String key, T value){
+    if ( ! map.containsKey(key) ) throw new RuntimeException(new IllegalAccessException("trying to set foreign key data"));
+    map.put(key, value);
+  }
+
+  public T get(String token){ return map.get(token); }
+
+  public void eraseToken(String token){ map.remove(token); }
+
+}
