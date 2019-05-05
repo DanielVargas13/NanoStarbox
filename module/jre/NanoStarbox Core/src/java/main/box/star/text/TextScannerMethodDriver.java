e5efef965d7c1dace924db1848f5413cd4b7de58
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-05 07:37:44 -0500

    TextScanner: Method Driver

--- module/jre/NanoStarbox Core/src/java/main/box/star/text/TextScannerMethodDriver.java
+++ module/jre/NanoStarbox Core/src/java/main/box/star/text/TextScannerMethodDriver.java
@@ -4,7 +4,39 @@ package box.star.text;
  * This interface hosts the text scanner method core operating functions.
  */
 public interface TextScannerMethodDriver {
+
+  /**
+   * Called by the TextScanner to signal that a new method call is beginning.
+   *
+   * @param context The safe use interface for context information.
+   * @param parameters the parameters given by the caller.
+   */
   void beginScanning(TextScannerMethodContext context, Object... parameters);
+
+  /**
+   * Signals whether or not the process should continue reading input.
+   *
+   * @param context The safe use interface for context information.
+   * @param input The string buffer.
+   * @return true if the TextScanner should read more input.
+   */
   boolean continueScanning(TextScannerMethodContext context, StringBuilder input);
+
+  /**
+   * Extended Operations Option
+   *
+   * This method is called when it is again safe to call seek/scan/next on the
+   * TextScanner.
+   *
+   * You can use this feature to create a virtual-pipe-chain.
+   *
+   * You can also (ideally) pre-process output, if having an exact copy of input
+   * data is not relevant for your purpose.
+   *
+   * @param scanner the TextScanner.
+   * @param scanned the input buffer.
+   * @return the scanned data as a string.
+   */
   String returnScanned(TextScanner scanner, StringBuilder scanned);
+
 }
