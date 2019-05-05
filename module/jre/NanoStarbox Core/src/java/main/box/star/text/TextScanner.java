1c9e85e1bd3b70e50de19ddb9b587d7d84922568
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-05 16:31:10 -0500

    TextScanner: MatchString: Quality Assurance 14

--- module/jre/NanoStarbox Core/src/java/main/box/star/text/TextScanner.java
+++ module/jre/NanoStarbox Core/src/java/main/box/star/text/TextScanner.java
@@ -498,7 +498,7 @@ public class TextScanner implements Iterable<Character>, Closeable {
    * Creates a method for scanning text.
    *
    */
-  public static class Method implements TextScannerMethod, Serializable, Cloneable {
+  public static class Method implements Serializable, Cloneable {
 
     long backup_index, backup_line, backup_column;
 
@@ -634,18 +634,46 @@ public class TextScanner implements Iterable<Character>, Closeable {
       }
     }
 
-    @Override
+    /**
+     * Called by the TextScanner to signal that a new method call is beginning.
+     *
+     * @param parameters the parameters given by the caller.
+     */
     public void startMethod(Object[] parameters) {}
 
-    @Override
+    /**
+     * Extended Operations Option
+     *
+     * This method is called when it is again safe to call seek/scan/next on the
+     * TextScanner.
+     *
+     * You can use this feature to create a virtual-pipe-chain.
+     *
+     * You can also (ideally) pre-process output, if having an exact copy of input
+     * data is not relevant for your purpose.
+     *
+     * @param scanner the TextScanner.
+     * @param scanned the input buffer.
+     * @return the scanned data as a string.
+     */
     public String computeMethodCall(TextScanner scanner, StringBuilder scanned) {
       return scanned.toString();
     }
 
-    @Override
+    /**
+     * Signals whether or not the process should continue reading input.
+     *
+     * @param input The string buffer.
+     * @return true if the TextScanner should read more input.
+     */
     public boolean continueScanning(StringBuilder input) { return true; }
 
-    @Override
+    /**
+     * Return true to break processing on this character.
+     *
+     * @param character
+     * @return false to continue processing.
+     */
     public boolean exitMethod(char character) { return character == 0; }
 
     @Override
