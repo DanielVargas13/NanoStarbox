351a7c0b97036848b58e7d624810f1f602ab80a8
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-09 17:19:21 -0500

    box.star.text.basic: Scanner: Macro Text Magic 9
    Macro in the hole

--- module/jre/NanoStarbox Core/src/java/test/box/star/text/basic/MacroScannerTest.java
+++ module/jre/NanoStarbox Core/src/java/test/box/star/text/basic/MacroScannerTest.java
@@ -6,14 +6,20 @@ import java.util.Stack;
 
 class MacroScannerTest {
 
-  Scanner scanner = new Scanner("test", "Java Menu: %(test \"%[JDK_HOME]\" %[JAVA_HOME])");
+  Scanner scanner = new Scanner("test", "%(menu %(list \"%[JDK_HOME]\" %[JAVA_HOME]))");
 
   @Test void main(){
     MacroScanner macroContext = new MacroScanner(System.getenv());
-    macroContext.addCommand("test", new MacroScanner.Command(){
+    macroContext.addCommand("list", new MacroScanner.Command(){
       @Override
       protected String run(String command, Stack<String> parameters) {
         return String.join(", ", parameters);
+      }
+    });
+    macroContext.addCommand("menu", new MacroScanner.Command(){
+      @Override
+      protected String run(String command, Stack<String> parameters) {
+        return "Java Menu: "+String.join(" ", parameters);
       }
     });
     System.out.println(macroContext.start(scanner));
