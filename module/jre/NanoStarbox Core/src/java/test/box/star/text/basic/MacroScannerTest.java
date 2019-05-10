411fdc44ba69c67b3a1ba9a4d693963845a21bd3
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-09 19:19:39 -0500

    box.star.text.basic: Scanner: Macro Text Magic 13
    Party in the back

--- module/jre/NanoStarbox Core/src/java/test/box/star/text/basic/MacroScannerTest.java
+++ module/jre/NanoStarbox Core/src/java/test/box/star/text/basic/MacroScannerTest.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 class MacroScannerTest {
 
-  Scanner scanner = new Scanner("test", "%(menu %(list \"'%[JDK_HOME]'\" \"'%[JAVA_HOME]'\"))");
+  Scanner scanner = new Scanner("test", "%(menu %(list '%[JDK_HOME]' '%[TEMP]'))");
 
   @Test void main(){
     MacroScanner macroContext = new MacroScanner(System.getenv());
@@ -29,7 +29,8 @@ class MacroScannerTest {
         StringBuilder out = new StringBuilder();
         out.append(call("alert", null));
         out.append(eval(" %[what]: \n\n"));
-        for (String p: split(parameters.firstElement())){
+        // spec: first element is variable list; platform: Windows; tweak variables for local system effects.
+        for (String p: parameters.firstElement().split(", ")){
           out.append("\t"+eval(p)+"\n");
         }
         return out.toString();
