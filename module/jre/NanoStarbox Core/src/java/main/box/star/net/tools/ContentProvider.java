6d4c3e5e8cf4a7268c417026117b01d45a404de3
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-14 22:30:21 -0500

    Content Provider: Local Path Mime Types Support

--- module/jre/NanoStarbox Core/src/java/main/box/star/net/tools/ContentProvider.java
+++ module/jre/NanoStarbox Core/src/java/main/box/star/net/tools/ContentProvider.java
@@ -3,12 +3,21 @@ package box.star.net.tools;
 import box.star.content.MimeTypeMap;
 import box.star.net.http.IHTTPSession;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContentProvider {
 
+  private static final Map<String, String> mimeTypePaths = new HashMap<>();
   private final MimeTypeMap mimeTypeMap;
   private final String baseUri;
 
+  final public void setPathMimeType(String path, String mimeType){
+    mimeTypePaths.put(path, mimeType);
+  }
+
   final public String getMimeType(String uri){
+    if (mimeTypePaths.containsKey(uri)) return mimeTypePaths.get(uri);
     return mimeTypeMap.get(mimeTypeMap.scanFileExtension(uri));
   }
 
