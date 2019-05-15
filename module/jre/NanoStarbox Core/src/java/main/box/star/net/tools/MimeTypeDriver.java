caea7dea52fe5b06818f34625613e93bbf888608
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-15 08:13:48 -0500

    Mini Docs

--- module/jre/NanoStarbox Core/src/java/main/box/star/net/tools/MimeTypeDriver.java
+++ module/jre/NanoStarbox Core/src/java/main/box/star/net/tools/MimeTypeDriver.java
@@ -4,6 +4,18 @@ import box.star.net.WebService;
 import box.star.net.tools.ServerContent;
 import box.star.net.tools.ServerResult;
 
+/**
+ * <p>{@link MimeTypeDriver}s create {@link ServerResult}s from {@link ServerContent} using a server environment such as the {@link WebService}.</p>
+ * <br>
+ * @param <SERVICE> the server environment
+ */
 public interface MimeTypeDriver<SERVICE> {
+  /**
+   * <p>Creates a mime-formatted {@link ServerResult} from a {@link ServerContent} source.</p>
+   * <br>
+   * @param server the server environment
+   * @param content the server content
+   * @return the mime formatted server result
+   */
   ServerResult createMimeTypeResult(SERVICE server, ServerContent content);
 }
