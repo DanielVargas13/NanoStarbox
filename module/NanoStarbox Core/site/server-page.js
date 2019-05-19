7de874ed2d76f0a6be966ef95fe671ec3c029322
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-19 16:45:56 -0500

    Project Restructuring 4

---
+++ module/NanoStarbox Core/site/server-page.js
@@ -0,0 +1,6 @@
+
+function result(){
+    return "this is output from my server-side-script-include";
+}
+
+var JSONObject = Packages.org.json.JSONObject;
---
+++ module/NanoStarbox Core/src/java/test/box/star/net/WebServerTest.java
@@ -0,0 +1,33 @@
+package box.star.net;
+
+//import box.star.net.tools.RhinoWebDriver;
+
+class WebServerTest {
+
+
+//  WebServer ws = new WebServer();
+//
+//  {
+//    ws.setDocumentRoot("src/java/resource/local/site/main");
+//    ws.makeSecure(new File("src/java/resource/test/Starbox.jks"), "Starbox");
+//  }
+//
+//  @Test
+//  void WebServer() throws Exception {
+//
+//    ws.start();
+//
+//    System.err.println("\nhttps://" + ws.getHost() + ":" + ws.getPort() + "/\n");
+//
+//    System.err.println(
+//
+//        "\tYou will get errors about certificates, because they are for development-purposes.\n" +
+//            "\tAccept them to continue development-testing.\n\n" +
+//
+//            "\tPress stop to quit."
+//    );
+
+//
+//System.in.read();
+//  }
+}
