37fd233bdbb1542fbc8fb30ec2c35714896aa1bb
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-21 07:22:58 -0500

    NSR9: no-nonsense: bin/cat

--- module/NanoStarbox Core/src/java/main/box/star/bin/cat.java
+++ module/NanoStarbox Core/src/java/main/box/star/bin/cat.java
@@ -7,14 +7,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class cat {
+  private static String INPUT_STREAM_SIGNAL = "-";
   public static void main(String[] args) {
-    for (String fileName : args) {
-      try {
-        System.out.print(Streams.readWholeString(new FileInputStream(new File(fileName))));
-      }
-      catch (IOException e) {
-        e.printStackTrace();
-      }
+    if (args.length == 0) args = new String[]{INPUT_STREAM_SIGNAL};
+    for (String fileName : args) doFile(fileName);
+  }
+  static private void doFile(String file){
+    try {
+      if (file.equals(INPUT_STREAM_SIGNAL)) Streams.transfer(System.in, System.out);
+      else Streams.transfer(new FileInputStream(new File(file)), System.out);
+    }
+    catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 }
