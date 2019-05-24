62975b1acff690e854a14adf91867612df65ce93
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-23 22:44:58 -0500

    Performance Talk

--- module/NanoStarbox Core/src/java/main/box/star/text/basic/Scanner.java
+++ module/NanoStarbox Core/src/java/main/box/star/text/basic/Scanner.java
@@ -20,7 +20,16 @@ import static box.star.text.Char.*;
  * error-reporting and sub-processing capabilities. The scanner also supports custom character-to-string translation
  * so that each scanner may provide standardized: `hard to define character error disambiguation`
  * to its users. In addition, the scanner supports overridable backslash escape expansion with
- * bi-level default implementation support.</p>
+ * bi-level default implementation support.</p><br>
+ * <p>The scanner automatically tracks backslash escape activation, and in some
+ * built-in scanner methods, automatically handles backslash expansion and output
+ * stream insertion. However the scanner does not usually provide such facilities,
+ * as the meaning of "character escape sequence" is often text format dependent.
+ * The design methodology of these features allows for them to be safely ignored
+ * by any custom implementation. The default implementation understands escaped
+ * and not escaped, according to the status of the previous character. This could cause
+ * logical errors in a text-stream that has not been correctly back-stepped (to the beginning
+ * of an escape sequence).</p>
  * <br>
  * <p>Quick Overview</p>
  * <ul>
