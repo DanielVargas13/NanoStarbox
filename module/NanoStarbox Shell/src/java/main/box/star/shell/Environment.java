69b4abd155cb77eb87522c829151f9f1024f627c
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-24 03:41:57 -0500

    Environment: System Independent Present Working Directory

--- module/NanoStarbox Shell/src/java/main/box/star/shell/Environment.java
+++ module/NanoStarbox Shell/src/java/main/box/star/shell/Environment.java
@@ -1,5 +1,7 @@
 package box.star.shell;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
@@ -7,10 +9,24 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * An implementation of map that can be used concurrently, and which can
- * serve as a map provider.
+ * <p>An implementation of map that can be used concurrently</p>
+ * <br>
+ * <p>This map can store objects as variables, and convert them to string on export.</p>
+ * <br>
+ * <p>This map supports system local present working directory environment variable configuration,
+ * and relative file path look-ups.</p>
+ * <br>
+ * <p>This map can mark variables for export (independent-copy) and can export them
+ * for internal use (with objects), or in serialized form (string-only).</p>
+ * <br>
  */
 public class Environment extends ConcurrentHashMap<String, Variable> implements EnvironmentExporter {
+
+  public final static String defaultCurrentDirectoryKey = "PWD";
+  public final static String defaultCurrentDirectory = System.getenv("user.dir");
+
+  protected String currentDirectoryKey;
+
   /**
    * <p>Creates an export copy of this Environment</p>
    * <br>
@@ -62,4 +78,30 @@ public class Environment extends ConcurrentHashMap<String, Variable> implements 
   {
     mapAllStrings(System.getenv());
   }
+
+  public void setSystemDirectoryKey(String currentDirectoryKey) {
+    this.currentDirectoryKey = currentDirectoryKey;
+  }
+
+  public String getCurrentDirectory() {
+    return get(defaultCurrentDirectoryKey).toString();
+  }
+
+  public void setCurrentDirectory(String currentDirectory){
+    if (new File(currentDirectory).exists())
+      throw new RuntimeException(new FileNotFoundException(currentDirectory));
+    Variable var = new Variable(currentDirectory);
+    var.export = true;
+    put(defaultCurrentDirectoryKey, var);
+    if (currentDirectoryKey != null) put(currentDirectoryKey, var);
+  }
+
+  {
+    setCurrentDirectory(defaultCurrentDirectory);
+  }
+
+  public File getRelativeFile(String name){
+    return new File(getCurrentDirectory(), name);
+  }
+
 }
