ba19d05ac063577d97306cc1f949dfb0fff704b0
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-05-24 13:59:35 -0500

    Basic Scanner: Bookmarking
    logical source order

--- module/NanoStarbox Core/src/java/main/box/star/text/basic/Scanner.java
+++ module/NanoStarbox Core/src/java/main/box/star/text/basic/Scanner.java
@@ -840,17 +840,17 @@ public class Scanner implements Closeable {
      this.quote = quoteSource(source.getPath());
      this.origin = compileToString();
     }
-    public String compileToString() {
+    private static String quoteSource(String source){
+      return source
+          .replaceAll(Char.toString(BACKSLASH), Char.toString(BACKSLASH, BACKSLASH))
+          .replaceAll(Char.toString(BACKSLASH, DOUBLE_QUOTE), Char.toString(BACKSLASH, DOUBLE_QUOTE));
+    }
+    private String compileToString() {
       return " location = " + "{line: " + line + ", column: " + column + ", index: " + index + ", source: \"" + quote + "\"};";
     }
     @Override
     public String toString() {
       return origin;
-    }
-    private static String quoteSource(String source){
-      return source
-          .replaceAll(Char.toString(BACKSLASH), Char.toString(BACKSLASH, BACKSLASH))
-          .replaceAll(Char.toString(BACKSLASH, DOUBLE_QUOTE), Char.toString(BACKSLASH, DOUBLE_QUOTE));
     }
   }
   /**
