b76cee4eaac3602eac18935f4bd548e70235457f
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-06-08 01:43:57 -0500

    Development Testbed

---
+++ module/NanoStarbox Shell/src/java/test/box/star/shell/ReadEvalPrintLoopPrototype.java
@@ -0,0 +1,18 @@
+package box.star.shell;
+
+import box.star.shell.script.Command;
+import box.star.shell.script.Interpreter;
+import box.star.text.basic.Scanner;
+
+import static box.star.text.Char.*;
+
+public class ReadEvalPrintLoopPrototype {
+  public static void main(String[] p){
+    Scanner x = new Scanner("/dev/stdin", System.in);
+    Command cmd;
+    System.err.println("type exit followed by enter on a single line to quit");
+    do {
+      cmd = Interpreter.parse(Command.class, x);
+    } while (! cmd.parameters.get(0).text.equals("exit"));
+  }
+}
